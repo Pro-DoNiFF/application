e1eeba1620d63e1ba97d9c5ab879ddad10c8ddfe
DoNiFF <prodoniff@gmail.com>
2023-08-11 15:31:48 +0300

    test

--- Main.cpp
+++ Main.cpp
@@ -6,9 +6,7 @@ int main()
 {
 	srand(time(NULL));
 	short health = 100;
-	short damages[] = { 5, 10, 15, 20, 25, 30, 50, 100 };
-
-	health -= damages[rand() % 8];
+	health -= 50;
 	std::cout << "Health: " << health << std::endl;
 	return 0;
 }
